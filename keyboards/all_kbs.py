f58a689014ed78c6d4784291850ea188e2212016
psi-func <shalaginmx998@yandex.ru>
2025-04-20 00:38:12 +0300

    + format kb

--- keyboards/all_kbs.py
+++ keyboards/all_kbs.py
@@ -6,11 +6,14 @@ def common_kb(tg_user_id: int):
     kb_list = [
         [KeyboardButton(text="Пройти программу на день")],
         [KeyboardButton(text="Изменить время рассылки")],
-        [KeyboardButton(text="Приостановить рассылку")]
+        [KeyboardButton(text="Приостановить рассылку")],
     ]
     if tg_user_id in admins:
         kb_list.append([KeyboardButton(text="⚙️ Админ панель")])
     keyboard = ReplyKeyboardMarkup(
-        keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True
+        keyboard=kb_list,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Воспользуйтесь меню:",
     )
     return keyboard
